4786a2d423ab30817ecf956f6b40a8d074f6dfbe
RepiElek <kyeepii@gmail.com>
2026-04-30 14:45:05 +0700

    pesan commit kamu

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'katalog_produk.dart';
-=======
-<<<<<<< HEAD
-import 'opening.dart'; // supaya SplashScreenAdmin dikenali
-=======
-import 'beranda.dart';
->>>>>>> 3efef7b76e7e06240bb5f2ce94d5ece0e7d62b65
->>>>>>> 3e1b5afeb8fd6bd1340c68cd3bf6f6abd72b0c9d
+import 'Katalog Produk.dart';
+import 'opening.dart';
+import 'Beranda.dart';
+import 'Kontak.dart' ;
 
 void main() {
   runApp(const MyApp());
@@ -20,31 +15,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      title: 'Frozen Food App',
-      home: const KatalogPage(),
-    );
-  }
-}
-=======
-<<<<<<< HEAD
       title: 'Refi Frozen Food',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreenAdmin(), // panggil splash screen
+      // Di sini kamu bisa pilih mau munculin apa dulu:
+      // 1. SplashScreenAdmin() -> Biasanya untuk pembuka
+      // 2. KatalogPage() -> Langsung ke daftar produk
+      // 3. Beranda() -> Menu utama
+      home: const SplashScreenAdmin(), 
     );
   }
 }
-=======
-      title: 'Frozen Food',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: const Beranda(),
-    );
-  }
-}
->>>>>>> 3efef7b76e7e06240bb5f2ce94d5ece0e7d62b65
->>>>>>> 3e1b5afeb8fd6bd1340c68cd3bf6f6abd72b0c9d
